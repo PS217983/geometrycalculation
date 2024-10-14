0c15250622625a6606600b4180a8a28585b0a78a
AnIcePancake <anicepancake@gmail.com>
2024-10-14 09:43:53 +0200

    unit test fail

--- tests/SystemsTest.php
+++ tests/SystemsTest.php
@@ -8,6 +8,6 @@ class SystemsTest extends TestCase
         $systems = new Systems;
         $this->assertEqualsWithDelta(0.005, $systems->CalculateUnits("dm", "hm", 5), 0.00001); 
         $this->assertEqualsWithDelta(13123.3595800525, $systems->CalculateUnits("km", "yd", 12), 0.00001);
-        $this->assertEqualsWithDelta(0.0789141414, $systems->CalculateUnits("in", "mi", 5000), 0.00000001);
+        $this->assertEqualsWithDelta(0.0789141414, $systems->CalculateUnits("in", "mi", 8000), 0.00000001);
        }
 }
